39f16e660ee05928c6d4bfeaa831d351973cc35e
Yago <yagowolf47@gmail.com>
2015-11-09 00:22:49 +0100

    login and register done

--- src/client/Client.scala
+++ src/client/Client.scala
@@ -4,6 +4,27 @@ import java.rmi.registry.Registry
 import java.rmi.registry.LocateRegistry
 
 object Client {
+
+  def loginRepl(stub: server.ServerTrait, t: Int): Option[String] = {
+    if (t < 0) {
+      println("Error, number of attempts exceeded")
+      System.exit(1)
+      None
+    } else {
+      val user = readLine("User: ")
+      val pass = readLine("Password: ")
+      val ret = stub login(user,pass)
+      if (ret) {
+        println("Welcome back! :D")
+        Some(user)
+      }
+      else {
+        println("Error, incorrect user or password ")
+        loginRepl(stub,t-1)
+      }
+    }
+  }
+
   def repl: Unit = {
     println("Que funcion quieres ejecutar?")
     println("[? for help] >>> ")
@@ -17,26 +38,42 @@ object Client {
     repl
   }
 
+  def createUserCli(stub: server.ServerTrait): Unit = {
+    val user = readLine("User: ")
+    val pass = readLine("User: ")
+    val ret = stub createUser(user,pass)
+
+    if (ret)
+      println("User created successfully")
+    else
+      println("Error! This user already exists! :(")
+  }
+
+  def starting(stub: server.ServerTrait): Unit = {
+    println("Welcome to Tweetorro terminal App!")
+    println("What do you want to do?")
+    val option = readLine("1 for login, 2 for register >>> ")
+    option match {
+      case "1" => loginRepl(stub,3)
+      case "2" =>
+        createUserCli(stub)
+        None
+      case _ =>
+        println("I dunno Understand...")
+        starting(stub)
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-	println("Hola sabroson!")
-	try {
-	  val registry = LocateRegistry getRegistry("192.168.1.5")
-	  val stub = registry.lookup("testservice").asInstanceOf[server.serverTrait]
+    println("Hola sabroson!")
+    try {
+      val registry = LocateRegistry getRegistry("localhost")
+      val stub = registry.lookup("tweetorro").asInstanceOf[server.ServerTrait]
 
-      repl
-	  // println("ASking for a key :D")
-
-	  // val query = stub giveFromRedis("mykey")
-      // query match {
-      //   case Some(value) => println("Got \""+value+"\"")
-      //   case None => println("This key does not exists")
-      // }
-
-      // println("Sending \"Hello world!\"")
-      // var response = stub echo("Hello world!")
-	  // println("Got \""+response+"\"")
-	} catch {
-	  case e: Exception => e printStackTrace
-	}
+      starting(stub)
+      // repl
+    }catch {
+      case e: Exception => e printStackTrace
+    }
   }
 }
---
+++ src/server/ServerTrait.scala
@@ -0,0 +1,16 @@
+package server
+
+import java.rmi.Remote
+import java.rmi.RemoteException
+
+trait ServerTrait extends Remote {
+
+  @throws(classOf[RemoteException])
+  def createUser(user: String, pass: String): Boolean
+
+  @throws(classOf[RemoteException])
+  def login(user: String, pass: String): Boolean
+
+}
+
+
