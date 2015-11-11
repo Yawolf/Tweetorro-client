4fb8c9c523bdc161298c9d12319840f8d5eaa9dd
Cazaril <eugenio90@gmail.com>
2015-11-11 19:48:24 +0100

    Client with errors. CAUTION: THIS CLIENT HAS ERRORS

--- src/client/Client.scala
+++ src/client/Client.scala
@@ -4,7 +4,7 @@ import java.rmi.registry.Registry
 import java.rmi.registry.LocateRegistry
 
 object Client {
-
+  
   def loginRepl(stub: server.ServerTrait, t: Int): Option[String] = {
     if (t < 0) {
       println("Error, number of attempts exceeded")
@@ -17,6 +17,7 @@ object Client {
       if (ret) {
         println("Welcome back! :D")
         Some(user)
+        
       }
       else {
         println("Error, incorrect user or password ")
@@ -25,22 +26,24 @@ object Client {
     }
   }
 
-  def repl: Unit = {
+  def repl(stub: server.ServerTrait, userLogged: String): Unit = {
     println("Que funcion quieres ejecutar?")
     println("[? for help] >>> ")
     val ln = readLine()
     ln match {
       case "?" =>
         println("echo\n query")
+      case "follow" =>
+        follow(stub,userLogged)
       case _ =>
         println("implementando!")
     }
-    repl
+    repl(stub, userLogged)
   }
 
   def createUserCli(stub: server.ServerTrait): Unit = {
     val user = readLine("User: ")
-    val pass = readLine("User: ")
+    val pass = readLine("Password: ")
     val ret = stub createUser(user,pass)
 
     if (ret)
@@ -48,11 +51,20 @@ object Client {
     else
       println("Error! This user already exists! :(")
   }
+  def follow(stub: server.ServerTrait,userLogged: String) : Unit = {
+    val userToFollow = readLine("User:")
+    
+    val ret = stub follow(userLogged,userToFollow)
+    if (ret)
+      println("User followed successfully")
+    else
+      println("Error! Something ocurred =( ")
+  }
 
   def starting(stub: server.ServerTrait): Unit = {
     println("Welcome to Tweetorro terminal App!")
     println("What do you want to do?")
-    val option = readLine("1 for login, 2 for register >>> ")
+    val option = readLine("1 for login, 2 for register, 3 for follow >>> ")
     option match {
       case "1" => loginRepl(stub,3)
       case "2" =>
@@ -70,8 +82,8 @@ object Client {
       val registry = LocateRegistry getRegistry("localhost")
       val stub = registry.lookup("tweetorro").asInstanceOf[server.ServerTrait]
 
-      starting(stub)
-      // repl
+      val userLogged = starting(stub)
+      repl(stub,userLogged)
     }catch {
       case e: Exception => e printStackTrace
     }
--- src/server/ServerTrait.scala
+++ src/server/ServerTrait.scala
@@ -10,7 +10,12 @@ trait ServerTrait extends Remote {
 
   @throws(classOf[RemoteException])
   def login(user: String, pass: String): Boolean
+  
+  @throws(classOf[RemoteException])
+	def setProfile(nombre: String, alias: String): Boolean
 
+	@throws(classOf[RemoteException])
+	def follow(userLogged:String, idUsuarioASeguir: String): Boolean
 }
 
 
