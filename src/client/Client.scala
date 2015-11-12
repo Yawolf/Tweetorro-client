2b62b8435338bf27d646d494f986de2cdf6cd354
Yago <yagowolf47@gmail.com>
2015-11-12 23:34:17 +0100

    state changes

--- src/client/Client.scala
+++ src/client/Client.scala
@@ -3,6 +3,7 @@ package client
 import java.rmi.registry.Registry
 import java.rmi.registry.LocateRegistry
 import scala.annotation.tailrec
+import scala.language.postfixOps
 
 sealed abstract class State
 object State {
@@ -12,30 +13,32 @@ object State {
   case object Logout extends State
   case object Stop extends State
   case object CreateUser extends State
+  case object ProfileState extends State
 }
 
+import State._
 object Client {
 
-  def login(stub: server.ServerTrait, t: Int): State = {
+  def login(stub: server.ServerTrait, t: Int): (State,String) = {
     if (t < 0) {
       println("Error, number of attempts exceeded")
-      Stop
+      (Stop,"")
     } else {
       val user = readLine("User: ")
       val pass = readLine("Password: ")
       val ret = stub login(user,pass)
       if (ret) {
         println("Welcome back! :D")
-        MainState
+        (MainState,user)
       }
       else {
         println("Error, incorrect user or password ")
-        loginRepl(stub,t-1)
+        login(stub,t-1)
       }
     }
   }
 
-  def createUserCli(stub: server.ServerTrait): State = {
+  def createUserCli(stub: server.ServerTrait): (State,String) = {
     val user = readLine("User: ")
     val pass = readLine("Password: ")
     val ret = stub createUser(user,pass)
@@ -45,34 +48,206 @@ object Client {
     else
       println("Error! This user already exists! :(")
 
-    StartState
+    (StartState,"")
   }
 
-  def startMenu(stub: server.ServerTrait): State = {
+  def startMenu(stub: server.ServerTrait, user: String): (State,String) = {
     println("What do you want to do?")
     val option = readLine("1 for login, 2 for register, e for exit >>> ")
     option match {
-      case "1" => Login
-      case "2" => CreateUser
-      case "e" => Stop
-      case _ =>
+      case "1" => (Login,"")
+      case "2" => (CreateUser,"")
+      case "e" => (Stop,"")
+      case _ => {
         println("I dunno Understand...")
-        StartState
+        (StartState,"")
+      }
     }
   }
 
-  def selectFunction(stub: server.ServerTrait): State = {
-    val f::args = readLine("[? for help] >>> ").split(" ")
+  def sendTweet(stub: server.ServerTrait, user: String, msg: List[String]):
+      (State,String) = {
+    if (msg.isEmpty)
+      println("Hey! You must write a message! :P")
+    else {
+      println("Sending you tweet...")
+      val message = msg mkString " "
+      val ret = stub sendTweet(user,message)
+      ret match {
+        case true => println("Tweew sent! :D")
+        case _ => println("Woops! Something went wrong :(")
+      }
+    }
+    (MainState,user)
+  }
+
+  def retweet(stub: server.ServerTrait, user: String, msg: List[String])
+      : (State,String) = {
+    if (msg.length != 1)
+      println("Hey! Give me a tweet ID!!")
+    else {
+      val tweetID = msg.head
+      println("Let's retweet this tweet: $tweetID")
+      val ret = stub retweet(user,tweetID)
+      ret match {
+        case true => println("The tweet was retweeted!! Yay! Party! :D")
+        case _ => println("Wow! Don't ask me why but... I cannot retweet that tweet")
+      }
+    }
+    (MainState,user)
+  }
+
+  def funcFollow(stub: server.ServerTrait, user: String, userID: List[String])
+      : (State,String) = {
+    if (userID.length != 1)
+      println("Just one argument bro, ONE ARGUMENT!")
+    else {
+      val ret = stub follow(user,userID.head)
+      ret match {
+        case true => println(s"You started following ${userID.head}!")
+        case _ => println("Are you sure that user does exist?")
+      }
+    }
+    (MainState,user)
+  }
+
+  def funcUnfollow(stub: server.ServerTrait, user: String, userID: List[String])
+      : (State,String) = {
+    if (userID.length != 1)
+      println("Just one argument bro, ONE ARGUMENT!")
+    else {
+      val ret = stub unfollow(user,userID.head)
+      ret match {
+        case true => println(s"You stoped following ${userID.head}!")
+        case _ => println("Are you sure that user does exist?")
+      }
+    }
+    (MainState,user)
+  }
+
+  def getFollows(f: String, stub: server.ServerTrait, user: String, n: Int)
+      : (State,String) = {
+    if (n <= 0)
+      println("Seriously? That's not a real Natural Number! Well Zero really is, but... please")
+    else {
+      
+      val ret = if (f == "followers")
+        stub followers(user,n)
+      else
+        stub following(user,n)
+      ret match {
+        case List() => println("So sad bro... No one! :'(")
+        case _ =>
+          ret.map(x => println(">> $x"))
+      }
+    }
+    (MainState,user)
+  }
+
+  def logout(stub: server.ServerTrait, user: String): (State,String) = {
+    val ret = stub logoutRemote(user)
+    ret match {
+      case true => {
+        println("User successfully loged out")
+        (Stop,user)
+      }
+      case _ => {
+        println("This message souldn't be showed never!")
+        (MainState, user)
+      }
+    }
+  }
+
+  def checkProfile(stub: server.ServerTrait, user: String, param: List[String])
+      : (State,String) = {
+    if (param.length != 1) {
+      println("Something went wrong.... We wrote help command just for somthing! USE IT!")
+      (ProfileState,user)
+    } else {
+      val paramStr = param mkString " "
+      val ret = stub checkRemoteProfile(user,paramStr)
+      ret match {
+        case Some(value) => {
+          println(s"@ >> $paramStr: $value")
+          (ProfileState, user)
+        }
+        case _ => {
+          println("Are you sure you know your own profile :P")
+          (ProfileState, user)
+        }
+      }
+    }
+  }
+
+  def modifyProfile(stub: server.ServerTrait, user: String, param: List[String])
+      : (State,String) = {
+    if (param.length != 1) {
+      println("Why you don't what to use the 'h' command? T_T")
+      (ProfileState, user)
+    } else {
+      val paramStr = param mkString " "
+      val ret = stub modifyRemoteProfile(user,paramStr)
+      ret match {
+        case true => {
+          println(s"@ >>>>>> $user modified with $paramStr")
+          (ProfileState, user)
+        }
+        case _ => {
+          println("Are you sure that field exists in your profile? xD")
+          (ProfileState, user)
+        }
+      }
+    }
+  }
+
+  def profileRepl(stub: server.ServerTrait, user: String): (State,String) = {
+    val f::tail = readLine("[? for help] @ >>> ").split(" ").toList
     f match {
-      case "getweets" => getTweets(stub,args)
-      case "tweet" => sendTweet(stub,args)
-      case "retweet" => retweet(stub,args)
-      case "follow" => follow(args)
-      case "unfollow" => unfollow(args)
-      case "following" => getFollowing(stub)
-      case "followers" => getFollowers(stub)
-      case "logout" => Stop
-      case "h" =>
+      case "check" => checkProfile(stub,user,tail)
+      case "modify" => modifyProfile(stub,user,tail)
+      case "h" => {
+        println("""Modifiying your profile eh? I can help you!
+    check <PARAM> => Check your actual PARAM value
+    modify <PARAM> => Modify your actual PARAM value
+        PARAMS:
+           username => Your user name (that's obvious, isn't it?)
+           realname => Your real name
+           passwd => Your password (only modify (obviously too (yeah! lisp style!)))
+           workst => Your current workstation, somethibg as... Awesomeness Inc.""")
+        (ProfileState,user)
+      }
+      case "e" => {
+        println("Leaving your profile settings!")
+        (MainState,user)
+      }
+      case _ => {
+        println("Sorry but... What did you said?")
+        (ProfileState,user)
+      }
+    }
+  }
+
+  def getTweets(stub: server.ServerTrait, user: String): (State,String) = {
+    (MainState,user)
+  }
+
+  def selectFunction(stub: server.ServerTrait,
+    user: String): (State,String) = {
+    val f::tail = readLine("[? for help] >>> ").split(" ").toList
+    f match {
+      case "getweets" => getTweets(stub,user)
+      case "tweet" => sendTweet(stub,user,tail)
+      case "retweet" => retweet(stub,user,tail)
+      case "follow" => funcFollow(stub,user,tail)
+      case "unfollow" => funcUnfollow(stub,user,tail)
+      case "following" => getFollows("following",stub,user,tail.head.toInt)
+      case "followers" => getFollows("followers",stub,user,tail.head.toInt)
+      case "profile" => (ProfileState,user)
+      case "logout" => {
+        logout(stub,user)
+        (Stop,user)
+      }
+      case "h" => {
         println("This is the help, YAY!")
         println("\tgetweets [N] => receive the first N tweets, default 10 :)")
         println("\ttweet <message> => Sends a new tweet!")
@@ -84,32 +259,35 @@ object Client {
         println("\tlogout => just... Logout :D")
         println("\th => show this help")
         println("\nAs you can se, I'm so easy to use ;D")
-        MainState
+        (MainState,user)
+      }
+      case _ => {
+        println("Have you thought what did you send me?")
+        (MainState,user)
+      }
     }
   }
 
   @tailrec
-  def repl(s: State): Unit = {
+  def repl(stub: server.ServerTrait, sa: (State, String)): Unit = {
     println("Welcome to Tweetorro terminal App!")
-    s match {
-      case StartState => repl(startMenu)
-      case Login => repl(login(stub, 3))
-      case CreateUser => repl(CreateUser(stub))
-      case MainState => repl(selectFunction(stub))
+    sa._1 match {
+      case StartState => repl(stub,startMenu(stub,""))
+      case Login => repl(stub,login(stub,3))
+      case CreateUser => repl(stub,createUserCli(stub))
+      case MainState => repl(stub,selectFunction(stub,sa._2))
+      case ProfileState => repl(stub,profileRepl(stub,sa._2))
       case Stop =>
         println("Bye!! See you soon! :D")
     }
   }
 
   def main(args: Array[String]): Unit = {
-    println("Hola sabroson!")
     try {
       val registry = LocateRegistry getRegistry("localhost")
       val stub = registry.lookup("tweetorro").asInstanceOf[server.ServerTrait]
-
-      starting(stub)
-      // repl
-    }catch {
+      repl(StartState,(stub,""))
+    } catch {
       case e: Exception => e printStackTrace
     }
   }
--- src/server/ServerTrait.scala
+++ src/server/ServerTrait.scala
@@ -3,6 +3,7 @@ package server
 import java.rmi.Remote
 import java.rmi.RemoteException
 
+
 trait ServerTrait extends Remote {
 
   @throws(classOf[RemoteException])
@@ -11,6 +12,30 @@ trait ServerTrait extends Remote {
   @throws(classOf[RemoteException])
   def login(user: String, pass: String): Boolean
 
+  @throws(classOf[RemoteException])
+  def sendTweet(user: String, message: String): Boolean
+
+  @throws(classOf[RemoteException])
+  def retweet(user: String, tweetID: String): Boolean
+
+  @throws(classOf[RemoteException])
+  def follow(user: String, userID: String): Boolean
+
+  @throws(classOf[RemoteException])
+  def unfollow(user: String, userID: String): Boolean
+
+  @throws(classOf[RemoteException])
+  def followers(user: String, number: Int): List[String]
+
+  @throws(classOf[RemoteException])
+  def following(user: String, number: Int): List[String]
+
+  @throws(classOf[RemoteException])
+  def logoutRemote(user: String): Boolean
+
+  @throws(classOf[RemoteException])
+  def checkRemoteProfile(user: String, param: String): Option[String]
+
+  @throws(classOf[RemoteException])
+  def modifyRemoteProfile(user: String, param: String): Boolean
 }
-
-
